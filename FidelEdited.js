e09a37a52b440d148a7d37bfb1551cbed684b75f
fidelity129 <31078526+fidelity129@users.noreply.github.com>
2020-06-05 04:01:07 +0100

    Update FidelEdited.js

--- FidelEdited.js
+++ FidelEdited.js
@@ -3,4 +3,4 @@ var greet = "hello world";
 var name = "UZOIGWE FIDEL ";
 var hngId = "hng-04523";
 var lang = "JavaScript";
-console.log (greet, ', '+ 'this is ' + name,' '+ ' with HNGi7 ID: ' + hngId,' ' + 'using ' + lang, ' ' + ' for stage 2 task  email' + email);
+console.log (greet, ', '+ 'this is ' + name,' '+ ' with HNGi7 ID: ' + hngId,' ' + 'using ' + lang, ' ' + ' for stage 2 task  email ' + email);
